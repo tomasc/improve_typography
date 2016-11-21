b2052bfd5d1f528c4025750e06e726e96fa4c3de
Tomas Celizna <tomas.celizna@gmail.com>
2016-11-21 19:57:12 +0100

    CS

--- lib/improve_typography.rb
+++ lib/improve_typography.rb
@@ -3,4 +3,6 @@ require 'improve_typography/processor'
 require 'improve_typography/processors/multiply_sign'
 require 'improve_typography/processors/quotes'
 
+require 'improve_typography/processors/cs/quotes'
+
 require 'improve_typography/version'
---
+++ lib/improve_typography/processors/cs/quotes.rb
@@ -0,0 +1,15 @@
+module ImproveTypography
+  module Processors
+    module CS
+      class Quotes < ImproveTypography::Processors::Quotes
+        def replace_double_quotes
+          str.gsub!(DOUBLE_QUOTES_REGEXP, '„\1“')
+        end
+
+        def replace_single_quotes
+          str.gsub!(SINGLE_QUOTES_REGEXP, '‚\1‘')
+        end
+      end
+    end
+  end
+end

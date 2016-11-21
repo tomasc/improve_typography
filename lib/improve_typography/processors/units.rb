f62527037e3a0a19d71f74be537b469cfe234ab7
Tomas Celizna <tomas.celizna@gmail.com>
2016-11-21 23:19:51 +0100

    optim

--- lib/improve_typography/processors/units.rb
+++ lib/improve_typography/processors/units.rb
@@ -1,10 +1,10 @@
 module ImproveTypography
   module Processors
     class Units < Processor
-      REGEXP = /(\d)(\s*)(m|mm|cm|km)(2|3)/
+      REGEXP = /(\d\s*)(m|mm|cm|km)(2|3)/
 
       def call
-        str.gsub(REGEXP, '\1\2\3<sup>\4</sup>')
+        str.gsub(REGEXP, '\1\2<sup>\3</sup>')
       end
     end
   end

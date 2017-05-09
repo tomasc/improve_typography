dda33175d9149388d1737a727b5baa5fc2d3110c
Tomáš Celizna <tomas.celizna@gmail.com>
2017-05-09 19:14:17 -0400

    improve nbsp

--- lib/improve_typography/processors/nbsp.rb
+++ lib/improve_typography/processors/nbsp.rb
@@ -2,7 +2,10 @@ module ImproveTypography
   module Processors
     class Nbsp < Processor
       def call
-        str.gsub(/(\s+)(.|\d+|.\.)(\s+)(.)/, '\1\2&nbsp;\4')
+        str
+          .gsub(/(\s+)(.|\d+|.\.)(\s+)(.)/, '\1\2&nbsp;\4') # in the middle of string
+          .gsub(/\A(.|\d+|.\.)(\s+)(.)/, '\1&nbsp;\3') # at the beginning of string
+          .gsub(/(\s+)(.|\d+|.\.)(\z|[\.?!]\z|[\.?!]\s)/, '&nbsp;\2\3') # at the end of string
       end
     end
   end
--- test/improve_typography/processors/nbsp_test.rb
+++ test/improve_typography/processors/nbsp_test.rb
@@ -7,6 +7,10 @@ module ImproveTypography
         it { Nbsp.call('and interdependent, sharing a common time.').must_equal 'and interdependent, sharing a&nbsp;common time.' }
         it { Nbsp.call('with 35 years of clips').must_equal 'with 35&nbsp;years of clips' }
         it { Nbsp.call('Andrew W. Mellon').must_equal 'Andrew W.&nbsp;Mellon' }
+        it { Nbsp.call('Width 1').must_equal 'Width&nbsp;1' }
+        it { Nbsp.call('Width 1? Hmm').must_equal 'Width&nbsp;1? Hmm' }
+        it { Nbsp.call('1 Width').must_equal '1&nbsp;Width' }
+        it { Nbsp.call('Hmm. 1 Width').must_equal 'Hmm. 1&nbsp;Width' }
       end
     end
   end

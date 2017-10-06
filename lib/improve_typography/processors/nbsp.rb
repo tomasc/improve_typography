8dd6ed9fe704d30d2fcb9c0d9a846fe0e1264103
Tomáš Celizna <tomas.celizna@gmail.com>
2017-10-06 20:21:09 +0200

    improve Nbsp

--- lib/improve_typography/processors/nbsp.rb
+++ lib/improve_typography/processors/nbsp.rb
@@ -3,9 +3,9 @@ module ImproveTypography
     class Nbsp < Processor
       def call
         str
-          .gsub(/(\s+)(.|\d+|.\.)(\s+)(.)/, '\1\2&nbsp;\4') # in the middle of string
-          .gsub(/\A(.|\d+|.\.)(\s+)(.)/, '\1&nbsp;\3') # at the beginning of string
-          .gsub(/(\s+)(.|\d+|.\.)(\z|[\.?!]\z|[\.?!]\s)/, '&nbsp;\2\3') # at the end of string
+          .gsub(/(\s+)(\S|\d+|\S\.)(\s+)(\S)/, '\1\2&nbsp;\4') # in the middle of string
+          .gsub(/\A(\w|\d+|\w\.)(\s+)(\S)/, '\1&nbsp;\3') # at the beginning of string
+          .gsub(/([^\S\n])(\S|\d+|\S\.)(\z|[\.?!]\z|[\.?!]\s)/, '&nbsp;\2\3') # at the end of string!!
           .gsub(/(\A|\s+)(©)(\s+)(\d+)/, '\1\2&nbsp;\3')
           .gsub(/(\A|\s+)(No\.|Vol\.)(\s+)(\d+)/, '\1\2&nbsp;\3')
       end
--- test/improve_typography/processors/nbsp_test.rb
+++ test/improve_typography/processors/nbsp_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 module ImproveTypography
   module Processors
     describe Nbsp do
-      describe 'single character' do
+      describe "do's" do
         it { Nbsp.call('and interdependent, sharing a common time.').must_equal 'and interdependent, sharing a&nbsp;common time.' }
         it { Nbsp.call('with 35 years of clips').must_equal 'with 35&nbsp;years of clips' }
         it { Nbsp.call('Andrew W. Mellon').must_equal 'Andrew W.&nbsp;Mellon' }
@@ -14,6 +14,11 @@ module ImproveTypography
         it { Nbsp.call('Title. © 2016').must_equal 'Title. ©&nbsp;2016' }
         it { Nbsp.call('Title, Vol. 2').must_equal 'Title, Vol.&nbsp;2' }
         it { Nbsp.call('Title, No. 3').must_equal 'Title, No.&nbsp;3' }
+      end
+
+      describe "dont's" do
+        it { Nbsp.call("\n2009").wont_equal "&nbsp;2009" }
+        it { Nbsp.call(", (2009)").wont_equal ",&nbsp;(2009)" }
       end
     end
   end

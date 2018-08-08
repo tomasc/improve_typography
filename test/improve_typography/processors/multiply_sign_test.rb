634ce9271cf264753128a84a5aa092e2019fe44f
Tomáš Celizna <tomas.celizna@gmail.com>
2018-08-08 17:29:10 +0200

    add multiply sign test

--- test/improve_typography/processors/multiply_sign_test.rb
+++ test/improve_typography/processors/multiply_sign_test.rb
@@ -10,6 +10,7 @@ module ImproveTypography
         it { MultiplySign.call('2 x 10').must_equal "2#{multiply_sign}10" }
         it { MultiplySign.call('a 200x1 b').must_equal "a 200#{multiply_sign}1 b" }
         it { MultiplySign.call('25,6 x 17.9').must_equal "25,6#{multiply_sign}17.9" }
+        it { MultiplySign.call('½ x ½').must_equal "½#{multiply_sign}½" }
       end
 
       describe "options" do

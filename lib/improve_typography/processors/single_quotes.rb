fa13ba30edae3f666bec14e81408ef83762aca63
asgerb <a@asgerbehnckejacobsen.dk>
2022-05-30 14:03:56 +0200

    Improve regexp

--- lib/improve_typography/processors/single_quotes.rb
+++ lib/improve_typography/processors/single_quotes.rb
@@ -3,18 +3,18 @@ module ImproveTypography
     class SingleQuotes < Processor
       def call
         return str unless sign_exists?(single_quotes)
-        return str unless str.match?(/[\'#{single_quotes[0]}#{single_quotes[1]}]/)
+        return str unless str.match?(regexp)
         replace_single_quotes
       end
 
       private
 
       def replace_single_quotes
-        str.gsub(regexp, "\\1#{single_quotes[0]}\\2#{single_quotes[1]}")
+        str.gsub(regexp, "#{single_quotes[0]}\\1#{single_quotes[1]}")
       end
 
       def regexp
-        @regexp ||= Regexp.new("(\\A|\\W)['#{single_quotes[0]}#{single_quotes[1]}](.*?)['#{single_quotes[0]}#{single_quotes[1]}](?!\\w)")
+        @regexp ||= Regexp.new("(?<=\\A|\\W)['#{single_quotes[0]}#{single_quotes[1]}](.*?)['#{single_quotes[0]}#{single_quotes[1]}](?!\\w)")
       end
 
       def single_quotes
